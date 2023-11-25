e3aaef978e9562fbdc2f8bbef5e1daedce7b9e76
Grant Cawley <grantlcawley@gmail.com>
2023-11-25 01:25:26 -0500

    arrow spin transition

--- scripts/default.js
+++ scripts/default.js
@@ -1,7 +1,11 @@
 function toggleNavDropdown() {
+	var arrow = document.getElementById("nav-arrow");
+	arrow.classList.toggle("up");
+	arrow.classList.toggle("down");
   var links = document.getElementsByClassName("nav-link");
   for (let i = 0; i < links.length; i++) {
     links[i].classList.toggle("open");
     links[i].classList.toggle("closed");
   }
+
 }
