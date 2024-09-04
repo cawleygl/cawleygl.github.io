d6637e991978160af6058b71ccab5c51a3c11cec
Grant Cawley <grantlcawley@gmail.com>
2024-09-04 16:36:42 -0400

    portfolio page

--- scripts/default.js
+++ scripts/default.js
@@ -6,46 +6,12 @@ function toggleNavDropdown() {
 	for (let i = 0; i < links.length; i++) {
 		links[i].classList.toggle("open");
 		links[i].classList.toggle("closed");
-		// links[i].classList.remove("invisible");
-		// links[i].classList.add("visible");
 	}
 
 }
 
-function activatePopup(element) {
-	element += "-popup";
-	var popup = document.getElementById(element);
-	popup.classList.remove("closed");
-	timeout = setTimeout(function () {
-		popup.classList.toggle("invisible");
-		popup.classList.toggle("visible");
-	}, 10);
-}
-
-function deactivatePopup(element) {
-	element += "-popup";
-	var popup = document.getElementById(element);
-	popup.classList.toggle("invisible");
-	popup.classList.toggle("visible");
-	timeout = setTimeout(function () {
-		popup.classList.add("closed");
-	}, 200);
-}
-
-function slideDrawer(element) {
-	var elements = document.getElementsByClassName(element);
-
-	var icon = elements[0];
-	var content = elements[1];
-
-	content.classList.add("slide-animation");
-	content.classList.toggle("play-slide-animation");
-
-	if (icon.innerHTML === "+") {
-		icon.innerHTML = "-";
-	} else {
-		icon.innerHTML = "+";
-	}
-
+function carouselNext() {
+	var links = document.getElementsByClassName("active");
+	console.log(links);
 }
 
